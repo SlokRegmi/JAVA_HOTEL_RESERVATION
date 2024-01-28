bc434ac46f7e4100bd454445f3b6e1958c1ef915
Slok Regmi <slokr86@gmail.com>
2024-01-28 22:33:41 +0545

    Setting bounds

--- App.java
+++ App.java
@@ -1,11 +1,5 @@
 import javax.swing.*;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPasswordField;
-import javax.swing.JTextField;
-import javax.swing.WindowConstants;
-
 /**
  * App
  */
@@ -15,18 +9,23 @@ public class App {
         JLabel  label1 = new JLabel("Username:");
         JLabel  label2 = new JLabel("Password:");
 
+JLabel title = new JLabel("<html><font face=\"Times New Roman\" size = \"5\">LOGIN PAGE</font></html>");
         JFrame f= new JFrame("Login Portal");
         JButton sub = new JButton("Login");
         JTextField email = new JTextField();
         JPasswordField pass = new JPasswordField();
         email.setColumns(25);
         pass.setEchoChar('*');
-        pass.setColumns(25);
+        pass.setColumns(25);    
+
+        //Adding components to the frame
+        title.setBounds(200, 20, 300, 50);
         label1.setBounds(20,80, 80, 30);
         label2.setBounds(20,150, 80, 30);
         email.setBounds(200,80,150,30);
         pass.setBounds(200, 150 , 150,30);
        sub.setBounds(100,220,80,30);
+       f.add(title);
         f.add(label1);
 
         f.add(email);
